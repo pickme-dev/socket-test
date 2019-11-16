478cefa7836cbae4c19c172161171bbf06e13ff1
emaitee <issatoyin@gmail.com>
2019-11-16 17:19:30 +0100

    change port to 58082

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require("express")();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
-var port = process.env.PORT || 4000;
+var port = process.env.PORT || 58082;
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -20,5 +20,5 @@ io.on("connection", function(socket) {
 });
 
 http.listen(port, function() {
-  console.log(`listening on *:${port}`);
+  console.log(`Listening on *:${port}`);
 });
